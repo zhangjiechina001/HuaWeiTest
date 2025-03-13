57648961aadef2abaa9a47b4104ca07cdd488b6d
ZhangJie <295737015@qq.com>
2025-03-13 08:08:50 +0800

    11

---
+++ 7.EnglistInput/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.EnglistInput
+{
+    class SrcData
+    {
+        public string Line1 { get; set; }
+
+        public string Line2 { get; set; }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SrcData srcData = GetSrcData();
+            List<string> list = GetAssociate(srcData);
+            Console.WriteLine(string.Join(" ",list));
+            Console.ReadKey();
+        }
+
+        static SrcData GetSrcData()
+        {
+            SrcData ret = new SrcData();
+            ret.Line1 = Console.ReadLine();
+            ret.Line2 = Console.ReadLine();
+            return ret;
+        }
+
+        static List<string> GetAssociate(SrcData data)
+        {
+            string[] list = data.Line1.Split(' ','\'');
+            string[] first = list.Where(t=>t.StartsWith(data.Line2)).OrderBy(t=>t.Length).ToArray();
+            return first.ToList();
+        }
+
+    }
+}
---
+++ StringTransMinString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTransMinString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static string MinString(string src)
+        {
+            var order = src.OrderBy(x => x).ToArray();
+            
+            int index = 0;
+            for (int i = 0; i < order.Length; i++)
+            {
+                if (src[i] != order[i]) 
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            int swapIndex = 0;
+            for(int i= or)
+        }
+    }
+}
